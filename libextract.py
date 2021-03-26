347c9346c1e34a907f2bdfd9f12d56441f8a1d45
marziehk <kooshkba@ualberta.ca>
2021-03-25 19:49:09 -0600

    typo error resolved

---
+++ libextract.py
@@ -0,0 +1,6 @@
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.preprocessing import PolynomialFeatures
